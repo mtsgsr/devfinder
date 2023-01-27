04415acafa0d6376a5a95bbb06b37370312194cc
Matheus Gomes <maatheus.gsr@gmail.com>
2023-01-27 15:41:46 -0300

    Added imports and passed state props to search and card

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className=""></div>;
+  const [search, setSearch] = React.useState("");
+
+  return (
+    <>
+      <Header />
+      <Search setSearch={setSearch} />
+      <Card search={search} />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
